fd1008c498c4dad896655e15de3da35ed9156dde
Mihaela-Adelina Coman <144226254+mihaelacoman1@users.noreply.github.com>
2025-08-09 20:18:24 +0300

    Update Task.java

--- project/Task.java
+++ project/Task.java
@@ -5,17 +5,15 @@ import java.net.Socket;
 public class Task {
     private String command;
     private Socket clientSocket;
-
     public Task(String command, Socket clientSocket) {
         this.command = command;
         this.clientSocket = clientSocket;
     }
-
     public String getCommand() {
         return command;
     }
-
     public Socket getClientSocket() {
         return clientSocket;
     }
 }
+
